543fb7b8895bf135cc05dc6da898385d07a52e9f
Richard <calin.richard@yahoo.com>
2023-06-16 13:38:00 +0300

    sonarlint

--- countries/src/main/java/com/zegasoftware/countries/model/dto/CountryDto.java
+++
@@ -1,4 +0,0 @@
-package com.zegasoftware.countries.model.dto;
-
-import java.util.List;
-public record CountryDto(int id, String name, String capital, List<String> timezones, String subregion, List<String> borders, int area, Integer population) {}
--- countries/src/main/java/com/zegasoftware/countries/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zegasoftware.countries.repository;
-
-import com.zegasoftware.countries.model.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-}
